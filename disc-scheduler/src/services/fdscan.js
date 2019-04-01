7cd311859d61d9b405dc667139c6471c9f9a8ed9
jBosak98 <you@example.com>
2019-04-01 12:05:00 +0200

    fdscan algorithm

--- disc-scheduler/src/services/fdscan.js
+++ disc-scheduler/src/services/fdscan.js
@@ -1,5 +1,36 @@
 export function fdscan(proc) {
-    return 0;
+    let i = 0;
+    let queue = [];
+    let time = 0;
+    let actual = undefined;
+    let headPosition = 0;
+    let headMovement = 0;
+    while (i < proc.length || queue.length !== 0) {
+        for (let p = i; p < proc.length; p++){
+            if (time >= proc[p].arrivalTime){
+                queue.push(proc[p]);
+                i++;
+            }
+        }
+        queue = queue.filter(function (task) {
+            return task.deadline >= Math.abs(headPosition - task.blockAddress) + time
+        });
+        if (queue.length !== 0) {
+            actual = queue[0];
+            queue.map(function (task) {
+                if (task.deadline - Math.abs(headPosition - task.blockAddress) + time < actual.deadline - Math.abs(headPosition - actual.blockAddress) + time){
+                    actual = task;
+                }
+            });
+            queue = queue.filter(function (task) {
+                return task !== actual;
+            });
+            headMovement += Math.abs(headPosition - actual.blockAddress);
+            headPosition = actual.blockAddress;
+        }
+        time++;
+    }
+    return headMovement
 }
 
 
