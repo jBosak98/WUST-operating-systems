3b255ff0dc2187e95c245affa4f8d3f38cdc8ece
jBosak98 <you@example.com>
2019-04-01 11:42:45 +0200

    edf algorithm

--- disc-scheduler/src/services/edf.js
+++ disc-scheduler/src/services/edf.js
@@ -1,3 +1,34 @@
 export function edf(proc){
-    return 0;
+    let i = 0;
+    let queue = [];
+    let time = 0;
+    let actual = undefined;
+    let headPosition = 0;
+    let headMovement = 0;
+    while (i < proc.length || queue.length !== 0) {
+        for (let p = i; p < proc.length; p++){
+            if (time >= proc[p].arrivalTime){
+                queue.push(proc[p]);
+                i++;
+            }
+        }
+        queue = queue.filter(function (task) {
+            return task.deadline >= Math.abs(headPosition - task.blockAddress) + time
+        });
+        if (queue.length !== 0) {
+            actual = queue[0];
+            queue.map(function (task) {
+                if (task.deadline < actual.deadline){
+                    actual = task;
+                }
+            });
+            queue = queue.filter(function (task) {
+                return task !== actual;
+            });
+            headMovement += Math.abs(headPosition - actual.blockAddress);
+            headPosition = actual.blockAddress;
+        }
+        time++;
+    }
+    return headMovement
 }
