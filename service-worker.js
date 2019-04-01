fa2f03dce3f5e7b5abcb3378e1b9e553245e26b6
jBosak98 <jbosak98@gmail.com>
2019-04-01 18:10:28 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/WUST-operating-systems/precache-manifest.75c0312d9a5810217bc41c24004e18f1.js"
+  "/WUST-operating-systems/precache-manifest.9c48eda727c21817172be0c302543606.js"
 );
 
 workbox.clientsClaim();
